1ec7f381e7207f93e1e6187f808919a423ff9296
Zach <zachary.hartfelder@outlook.com>
2023-11-03 21:02:49 -0500

    found avrdude-libc is built in to platformio

---
+++ include/definitions.h
@@ -0,0 +1,22 @@
+#ifndef _DEFINITIONS_H_
+#define _DEFINITIONS_H_
+
+#define LED_PIN     (1<<7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
-//#include <Arduino.h>
+
+#include "definitions.h"
 #include <stdio.h>
 #include <util/delay.h>
 #include <avr/io.h>
 
 
-#define LED_PIN (1<<7)
+#define     LED_PIN (1<<7)
 
 
 int main(void) {
     // setup
+    
     
     // sets the pin for the LED to output
     DDRC |= LED_PIN;
@@ -19,7 +21,7 @@ int main(void) {
     while(1) {
         // flips the output pin for the LED
         PORTC ^= LED_PIN;
-        _delay_ms(1000);
+        _delay_ms(2000);
         
         //printf("hello world");
     }
